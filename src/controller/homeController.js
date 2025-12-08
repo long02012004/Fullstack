98727a8295c5523c23d4ac2586a6058b3c5731af
qLong <nguyenquanglong100285@gmail.com>
2025-12-08 17:38:38 +0700

    tách ra mô hình MVC

---
+++ src/controller/homeController.js
@@ -0,0 +1,7 @@
+const handleHelloWorld = (req, res) => {
+  return res.render("home.ejs");
+};
+const handleUserPage = (req, res) => {
+  return res.render("user.ejs");
+};
+export { handleHelloWorld, handleUserPage };
--- src/routes/web.js
+++ src/routes/web.js
@@ -1,10 +1,13 @@
 // dùng để cấu hình các route cho giao diện web
 import express from "express";
+import {
+  handleHelloWorld,
+  handleUserPage,
+} from "../controller/homeController.js";
 const router = express.Router();
 const initWebRoutes = (app) => {
-    router.get("/", (req, res) => {
-        return res.send("Hello from Home Page");
-    });
-    return app.use("/", router);
+  router.get("/", handleHelloWorld);
+  router.get("/user", handleUserPage);
+  return app.use("/", router);
 };
 export default initWebRoutes;
