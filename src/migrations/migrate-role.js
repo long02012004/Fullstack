d0dcac62b377bea9c01e57d53b1748e816389fcb
qLong <nguyenquanglong100285@gmail.com>
2025-12-26 16:46:05 +0700

    Tạo Models/Tables Cho Dự Án

--- src/migrations/migrate-role.js
+++ src/migrations/migrate-role.js
@@ -2,20 +2,17 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable("Users", {
+    await queryInterface.createTable("Role", {
       id: {
         allowNull: false,
         autoIncrement: true,
-        primaryKey: true,
+        primaryKey: true,   
         type: Sequelize.INTEGER,
       },
-      email: {
+      url: {
         type: Sequelize.STRING,
       },
-      password: {
-        type: Sequelize.STRING,
-      },
-      username: {
+      description: {
         type: Sequelize.STRING,
       },
       createdAt: {
@@ -29,6 +26,6 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable("Users");
+    await queryInterface.dropTable("Role");
   },
 };
